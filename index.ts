52705e9ba9135235d338dc9f56331923d87e219f
Fulvio Cusimano <fulviocusimano@Fulvios-MacBook-Pro.local>
2021-07-16 12:21:48 +0200

    first commit

---
+++ index.ts
@@ -0,0 +1,20 @@
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResultV2,
+  Handler,
+} from 'aws-lambda';
+import * as _ from 'lodash';
+
+const handler: Handler = async (
+  // eslint-disable-next-line no-unused-vars
+  event: APIGatewayProxyEvent,
+): Promise<APIGatewayProxyResultV2> => {
+  const val = _.random(10);
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(`This is random: ${val}`),
+  };
+  return response;
+};
+
+export default handler;
