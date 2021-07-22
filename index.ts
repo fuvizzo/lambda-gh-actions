2f2fd8cf158539d53e4171e390f21195d6417ddc
Fulvio Cusimano <fulviocusimano@Fulvios-MacBook-Pro.local>
2021-07-22 12:07:55 +0200

    fixing deployment steps

--- index.ts
+++
@@ -1,9 +0,0 @@
-import { Handler } from 'aws-lambda';
-
-import app from './src/koa';
-
-const serverless = require('aws-serverless-koa');
-
-const handler: Handler = serverless(app);
-
-export default handler;
