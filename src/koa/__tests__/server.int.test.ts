0a7255c2fb6a5edf8d039831a140375675dede68
Fulvio Cusimano <fulviocusimano@Fulvios-MacBook-Pro.local>
2021-08-04 19:13:18 +0200

    added swagger support

--- src/koa/__tests__/server.int.test.ts
+++ src/koa/__tests__/server.int.test.ts
@@ -2,26 +2,57 @@
 import http from 'http';
 import request from 'supertest';
 import koaApp from '..';
+import AppError from '../../error/app-error';
 
 const app = request(http.createServer(koaApp.callback()));
 const rootPath = process.env.API_ROOT_PATH;
 
+expect(new AppError('', 500)).toBeInstanceOf(Error);
+
 describe('APIs - unit tests', () => {
-  describe('"test get" endpoint', () => {
+  describe('"healthcheck" endpoint', () => {
     it('succesfully returns a 200 response', async () => {
-      const res = await app.get(`/${rootPath}/`);
+      const res = await app.get(`/${rootPath}/healthcheck`);
       expect(res.body).toBeInstanceOf(Object);
-      expect(res.body.value).toEqual('Hello World!');
+      expect(res.body.message).toEqual('OK');
       expect(res.statusCode).toEqual(200);
     });
   });
 
-  describe('"test get" endpoint with "id" param', () => {
+  describe('Root endpoint', () => {
     it('succesfully returns a 200 response', async () => {
-      const res = await app.get(`/${rootPath}/some-id`);
-      expect(res.body).toBeInstanceOf(Object);
-      expect(res.body.value).toEqual('some-id');
+      const res = await app.get(`/${rootPath}/`);
+      expect(res.text).toEqual('Hello World!');
       expect(res.statusCode).toEqual(200);
+    });
+  });
+
+  describe('"Users" endpoint', () => {
+    describe('"get"', () => {
+      it('succesfully returns a 200 response', async () => {
+        const res = await app.get(`/${rootPath}/users`);
+        expect(res.body).toBeInstanceOf(Array);
+        expect(res.body.length).toEqual(2);
+        expect(res.statusCode).toEqual(200);
+      });
+    });
+
+    describe('"get" with mathing "id" param', () => {
+      it('succesfully returns a 200 response', async () => {
+        const res = await app.get(`/${rootPath}/users/foo`);
+        expect(res.body).toBeInstanceOf(Object);
+        expect(res.body.id).toEqual('foo');
+        expect(res.body.name).toEqual('Foo');
+        expect(res.statusCode).toEqual(200);
+      });
+    });
+
+    describe('"get" with wrong "id" param', () => {
+      it('succesfully returns a 404 response', async () => {
+        const res = await app.get(`/${rootPath}/users/some-id`);
+        expect(res.body).toBeInstanceOf(Object);
+        expect(res.statusCode).toEqual(404);
+      });
     });
   });
 });
--- src/koa/index.ts
+++ src/koa/index.ts
@@ -1,20 +1,87 @@
 import Koa, { ParameterizedContext, Next } from 'koa';
+import yamljs from 'yamljs';
 import Router from 'koa-router';
 
+import { koaSwagger, SwaggerOptions } from 'koa2-swagger-ui';
+import AppError from '../error/app-error';
+
+interface KoaSwaggerOptions extends SwaggerOptions {
+  host?: string;
+  basePath?: string;
+}
+
+const apiPrefix = `/${process.env.API_ROOT_PATH}`;
+
 const app = new Koa();
+
 const router = new Router({
-  prefix: `/${process.env.API_ROOT_PATH}`,
+  prefix: apiPrefix,
+});
+const spec: KoaSwaggerOptions = yamljs.load('./swagger.yaml');
+spec.host = process.env.HOST;
+spec.basePath = `/${process.env.API_ROOT_PATH}`;
+
+app.use(koaSwagger({ swaggerOptions: { spec } }));
+
+const userRouter = new Router({
+  prefix: '/users',
 });
 
-router.get('/:id', (ctx: ParameterizedContext, next: Next) => {
-  const { id } = ctx.params;
-  console.log(`URL --> ${ctx.request.url}`);
-  ctx.body = { value: id };
+router.get('/healthcheck', async (ctx: ParameterizedContext, next: Next) => {
+  const healthcheck = {
+    uptime: process.uptime(),
+    message: 'OK',
+    timestamp: Date.now(),
+  };
+  ctx.body = healthcheck;
+  try {
+    // TODO: add further things to check (e.g. connecting to dababase)
+    // throw new Error('Service unavailable');
+  } catch (e) {
+    healthcheck.message = e.message;
+    ctx.status = 503;
+  }
+  await next();
 });
 
 router.get('/', (ctx: ParameterizedContext, next: Next) => {
-  ctx.body = { value: 'Hello World!' };
+  ctx.body = 'Hello World!';
 });
+
+userRouter.get('/', (ctx: ParameterizedContext, next: Next) => {
+  ctx.body = [
+    { id: 'boo', name: 'Boo' },
+    { id: 'foo', name: 'Foo' },
+  ];
+});
+
+userRouter.get('/:id', (ctx: ParameterizedContext, next: Next) => {
+  const { id } = ctx.params;
+  console.log(`URL --> ${ctx.request.url}`);
+  if (id === 'foo') {
+    ctx.body = { id, name: 'Foo' };
+    next();
+  } else {
+    throw new AppError('User not found', 404);
+  }
+});
+
+userRouter.delete('/:id', (ctx: ParameterizedContext, next: Next) => {
+  throw new AppError('Service not implemented', 501);
+});
+
+// Error handling middleware
+app.use(async (ctx: ParameterizedContext, next: Next) => {
+  try {
+    await next();
+  } catch (err) {
+    ctx.status = err.statusCode;
+    ctx.body = err.message;
+    // ctx.app.emit('error', err, ctx);
+  }
+});
+
+router.use(userRouter.routes(), userRouter.allowedMethods());
 
 app.use(router.routes()).use(router.allowedMethods());
 
